59b15b3403d38275be0bb75c0a362d527ceec785
ivantoribio <al428068@uji.es>
2025-10-07 15:50:11 +0200

    Hecho ej 1 y 2 no del todo

--- Practica1.java
+++ Practica1.java
@@ -6,14 +6,61 @@ public class Practica1 {
 
     //EJERCICIO 1
     public static Set<Integer> multiplos (Iterator<Integer> it) {
-        //TODO
-        return null;
+        List<Integer> elementos = new ArrayList<>();
+        Set<Integer> resultado = new HashSet<>();
+        while (it.hasNext()) {
+            int n = it.next();
+            if (n != 0) {
+                elementos.add(n);
+            }
+        }
+
+        for (int i = 0; i < elementos.size(); i++) {
+            int actual = elementos.get(i);
+            for (int j = 0; j < elementos.size(); j++) {
+                if (i != j) {
+                    int otro = elementos.get(j);
+                    if (otro != 0 && actual % otro == 0) {
+                        resultado.add(actual);
+                        break;
+                    }
+                }
+            }
+        }
+        return resultado;
     }
 
     //EJERCICIO2
     public static void separate (Set<Integer> cuadrados, Set<Integer> noCuadrados)  {
-        //TODO
+        Set<Integer> union = new HashSet<>(cuadrados);
+        union.addAll(noCuadrados);
 
+        Set<Integer> posiblesRaices = new HashSet<>();
+        for (int y : union) {
+            posiblesRaices.add(y);
+        }
+
+        Set<Integer> nuevosCuadrados = new HashSet<>();
+        Set<Integer> nuevosNoCuadrados = new HashSet<>();
+        for (int x : union) {
+            boolean esCuadrado = false;
+            for (int y : posiblesRaices) {
+                if (y * y == x) {
+                    esCuadrado = true;
+                    break;
+                }
+            }
+            if (esCuadrado) {
+                nuevosCuadrados.add(x);
+            } else {
+                nuevosNoCuadrados.add(x);
+            }
+        }
+
+        cuadrados.clear();
+        noCuadrados.clear();
+        cuadrados.addAll(nuevosCuadrados);
+        noCuadrados.addAll(nuevosNoCuadrados);
     }
 
     //EJERCICIO 3
